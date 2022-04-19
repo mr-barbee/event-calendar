dd7733c5cd2bb36d0bf6c69a56e0df988eb3cd88
Jibril <admin@hostenance.com>
2022-04-19 00:56:34 -0400

    Added the query for gathering the events

--- backend/web/modules/custom/ec_core/src/Plugin/GraphQL/SchemaExtension/ComposableSchemaExampleExtension.php
+++ backend/web/modules/custom/ec_core/src/Plugin/GraphQL/SchemaExtension/ComposableSchemaExampleExtension.php
@@ -6,7 +6,11 @@ use Drupal\graphql\GraphQL\ResolverBuilder;
 use Drupal\graphql\GraphQL\ResolverRegistryInterface;
 use Drupal\graphql\GraphQL\Response\ResponseInterface;
 use Drupal\graphql\Plugin\GraphQL\SchemaExtension\SdlSchemaExtensionPluginBase;
-use Drupal\ec_core\GraphQL\Response\ArticleResponse;
+use Drupal\ec_core\Wrappers\QueryConnection;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\user\Entity\User;
 
 /**
  * @SchemaExtension(
@@ -24,64 +28,31 @@ class ComposableSchemaExampleExtension extends SdlSchemaExtensionPluginBase {
   public function registerResolvers(ResolverRegistryInterface $registry): void {
     $builder = new ResolverBuilder();
 
-    $registry->addFieldResolver('Query', 'article',
-      $builder->produce('entity_load')
-        ->map('type', $builder->fromValue('node'))
-        ->map('bundles', $builder->fromValue(['article']))
-        ->map('id', $builder->fromArgument('id'))
-    );
-
-    // Create article mutation.
-    $registry->addFieldResolver('Mutation', 'createArticle',
-      $builder->produce('create_article')
-        ->map('data', $builder->fromArgument('data'))
-    );
-
-    $registry->addFieldResolver('ArticleResponse', 'article',
-      $builder->callback(function (ArticleResponse $response) {
-        return $response->article();
-      })
-    );
-
-    $registry->addFieldResolver('ArticleResponse', 'errors',
-      $builder->callback(function (ArticleResponse $response) {
-        return $response->getViolations();
-      })
-    );
-
-    $registry->addFieldResolver('Article', 'id',
-      $builder->produce('entity_id')
-        ->map('entity', $builder->fromParent())
-    );
-
-    $registry->addFieldResolver('Article', 'title',
-      $builder->compose(
-        $builder->produce('entity_label')
-          ->map('entity', $builder->fromParent())
-      )
-    );
-
-    $registry->addFieldResolver('Article', 'author',
-      $builder->compose(
-        $builder->produce('entity_owner')
-          ->map('entity', $builder->fromParent()),
-        $builder->produce('entity_label')
-          ->map('entity', $builder->fromParent())
-      )
-    );
-
+    /**
+     * Get the current user data for the logged in user
+     */
     $registry->addFieldResolver('Query', 'currentUser', $builder->compose(
       $builder->produce('get_user'),
       $builder->produce('entity_load')
         ->map('type', $builder->fromValue('user'))
         ->map('id', $builder->fromParent()),
       $builder->callback(function ($entity) {
-        // Here we can do anything we want to the data. We get as a parameter anyting that was returned
-        // in the previous step.
+        foreach($entity->get('field_user_categories')->getValue() as $taxonamy) {
+          $categories[$taxonamy['target_id']] = Term::load($taxonamy['target_id'])->get('name')->value;
+        }
+        foreach($entity->get('field_user_experience_skills')->getValue() as $taxonamy) {
+          $experiences[$taxonamy['target_id']] = Term::load($taxonamy['target_id'])->get('name')->value;
+        }
         return [
           'id' => $entity->id(),
           'name' => $entity->getDisplayName(),
-          'full_name' => $entity->field_user_full_name->value,
+          'fullName' => $entity->field_user_full_name->value,
+          'phone' => $entity->field_user_phone->value,
+          'email' => $entity->field_user_primary_contact->value,
+          'contact' => $entity->field_user_contact->value,
+          'categories' => $categories ?? [],
+          'experiences' => $experiences ?? [],
+          'note' => $entity->field_user_note->value
         ];
       })
     ));
@@ -89,11 +60,134 @@ class ComposableSchemaExampleExtension extends SdlSchemaExtensionPluginBase {
 
 
 
+
+
+
+
+    /**
+     * Get the event data for each events types in the system.
+     */
+    $registry->addFieldResolver('Query', 'event',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('node'))
+        ->map('bundles', $builder->fromValue(['event']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+
+    $registry->addFieldResolver('Event', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Event', 'title',
+      $builder->compose(
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('uppercase')
+          ->map('string', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('Event', 'body',
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('body.value'))
+    );
+
+    $registry->addFieldResolver('Event', 'startDate', $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node:event'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_event_start_date')),
+      $builder->callback(function ($entity) {
+        // need to adjust the string for the timezone so -00:00 is needed
+        $dateTime = new DrupalDateTime($entity[0]['value'] . '-00:00');
+        return \Drupal::service('date.formatter')->format($dateTime->getTimestamp(), 'long');
+      })
+    ));
+
+    $registry->addFieldResolver('Event', 'endDate', $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node:event'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_event_end_date')),
+      $builder->callback(function ($entity) {
+                // need to adjust the string for the timezone so -00:00 is needed
+        $dateTime = new DrupalDateTime($entity[0]['value'] . '-00:00');
+        return \Drupal::service('date.formatter')->format($dateTime->getTimestamp(), 'long');
+      })
+    ));
+
+    $registry->addFieldResolver('Event', 'categories', $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node:event'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_event_volunteer_categories')),
+      $builder->callback(function ($entity) {
+        foreach ( $entity as $element ) {
+          $paragraph = Paragraph::load( $element['target_id'] );
+          $categories[] = [
+            'category' => Term::load($paragraph->field_event_category->target_id)->get('name')->value,
+            'count' => $paragraph->field_event_category_count->value
+          ];
+        }
+        return $categories ?? [];
+      })
+    ));
+
+    $registry->addFieldResolver('Event', 'volunteers', $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node:event'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_event_volunteers')),
+      $builder->callback(function ($entity) {
+        foreach ( $entity as $element ) {
+          // Load the paragraph and user.
+          $paragraph = Paragraph::load( $element['target_id'] );
+          $user = User::load($paragraph->field_event_volunteer->target_id);
+          // And the user to the array.
+          if (!empty($user)) {
+            $volunteers[] = [
+              'volunteer' => $user->getDisplayName(),
+              'hours' => $paragraph->field_event_volunteer_hrs_avail->value,
+              'note' => $paragraph->field_event_volunteer_notes->value
+            ];
+          }
+        }
+        return $volunteers ?? [];
+        })
+    ));
+
+
+
+    /**
+     * An event query to get all the events for a specific range and date
+     * time stamp
+     */
+    $registry->addFieldResolver('Query', 'events',
+      $builder->produce('query_events')
+        ->map('offset', $builder->fromArgument('offset'))
+        ->map('limit', $builder->fromArgument('limit'))
+    );
+
+    $registry->addFieldResolver('EventConnection', 'total',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->total();
+      })
+    );
+
+    $registry->addFieldResolver('EventConnection', 'items',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->items();
+      })
+    );
+
     // Response type resolver.
-    $registry->addTypeResolver('Response', [
-      __CLASS__,
-      'resolveResponse',
-    ]);
+    // $registry->addTypeResolver('Response', [
+    //   __CLASS__,
+    //   'resolveResponse',
+    // ]);
   }
 
   /**
@@ -108,12 +202,12 @@ class ComposableSchemaExampleExtension extends SdlSchemaExtensionPluginBase {
    * @throws \Exception
    *   Invalid response type.
    */
-  public static function resolveResponse(ResponseInterface $response): string {
-    // Resolve content response.
-    if ($response instanceof ArticleResponse) {
-      return 'ArticleResponse';
-    }
-    throw new \Exception('Invalid response type.');
-  }
+  // public static function resolveResponse(ResponseInterface $response): string {
+  //   // Resolve content response.
+  //   if ($response instanceof ArticleResponse) {
+  //     return 'ArticleResponse';
+  //   }
+  //   throw new \Exception('Invalid response type.');
+  // }
 
 }
